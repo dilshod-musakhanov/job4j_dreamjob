d14ecf59887861ddfc99f90701c11073dc972cf8
Dilshod Musakhanov <musakhanov@yahoo.com>
2022-12-12 07:12:59 +0400

    Список кандидатов [#504842]

---
+++ src/main/java/ru/job4j/dreamjob/model/CandidateStore.java
@@ -0,0 +1,27 @@
+package ru.job4j.dreamjob.model;
+
+import java.time.LocalDate;
+import java.util.Collection;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+
+public class CandidateStore {
+
+    private static final CandidateStore INST = new CandidateStore();
+
+    private final Map<Integer, Candidate> candidates = new ConcurrentHashMap<>();
+
+    private CandidateStore() {
+        candidates.put(1, new Candidate(1, "Anna", "1 year experience", LocalDate.now()));
+        candidates.put(2, new Candidate(2, "Garik", "3 years experience", LocalDate.now()));
+        candidates.put(3, new Candidate(3, "Azamat", "5 years experience", LocalDate.now()));
+    }
+
+    public static CandidateStore instOf() {
+        return INST;
+    }
+
+    public Collection<Candidate> findAll() {
+        return candidates.values();
+    }
+}
