61299dfe9931da174daffb1c3726fd11a3b53632
Dilshod Musakhanov <musakhanov@yahoo.com>
2022-12-12 10:48:30 +0400

    Список кандидатов [#504842 #370195]

--- src/main/java/ru/job4j/dreamjob/model/Candidate.java
+++ src/main/java/ru/job4j/dreamjob/model/Candidate.java
@@ -1,28 +1,23 @@
 package ru.job4j.dreamjob.model;
 
-import java.time.LocalDate;
 import java.time.LocalDateTime;
-import java.time.format.DateTimeFormatter;
 import java.util.Objects;
 
 public class Candidate {
     private int id;
     private String name;
     private String description;
-    private String created;
-
-    private final LocalDateTime now = LocalDateTime.now();
-    private final DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
+    private LocalDateTime created;
 
     public Candidate() {
 
     }
 
-    public Candidate(int id, String name, String description) {
+    public Candidate(int id, String name, String description, LocalDateTime created) {
         this.id = id;
         this.name = name;
         this.description = description;
-        this.created = now.format(formatter);
+        this.created = created;
     }
 
     public int getId() {
@@ -49,11 +44,11 @@ public class Candidate {
         this.description = description;
     }
 
-    public String getCreated() {
+    public LocalDateTime getCreated() {
         return created;
     }
 
-    public void setCreated(String created) {
+    public void setCreated(LocalDateTime created) {
         this.created = created;
     }
 
--- src/main/java/ru/job4j/dreamjob/model/CandidateStore.java
+++ src/main/java/ru/job4j/dreamjob/model/CandidateStore.java
@@ -1,5 +1,6 @@
 package ru.job4j.dreamjob.model;
 
+import java.time.LocalDateTime;
 import java.util.Collection;
 import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
@@ -11,9 +12,9 @@ public class CandidateStore {
     private final Map<Integer, Candidate> candidates = new ConcurrentHashMap<>();
 
     private CandidateStore() {
-        candidates.put(1, new Candidate(1, "Anna", "1 year experience"));
-        candidates.put(2, new Candidate(2, "Garik", "3 years experience"));
-        candidates.put(3, new Candidate(3, "Azamat", "5 years experience"));
+        candidates.put(1, new Candidate(1, "Anna", "1 year experience", LocalDateTime.now()));
+        candidates.put(2, new Candidate(2, "Garik", "3 years experience", LocalDateTime.now()));
+        candidates.put(3, new Candidate(3, "Azamat", "5 years experience", LocalDateTime.now()));
     }
 
     public static CandidateStore instOf() {
